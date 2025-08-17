55e5da8aeda1cd6653e1241f56d8cc0be575573d
Sarah Salah <sarah.salah@websquids.com>
2025-08-17 13:24:05 +0300

    fix

--- squid-cms/config/server.ts
+++ squid-cms/config/server.ts
@@ -1,7 +1,33 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+import path from "path";
+
+// Define the type for the 'env' function
+type EnvFunction = {
+  (key: string): string | undefined; // for strings
+  bool: (key: string) => boolean; // for booleans
+  int: (key: string) => number; // for integers
+};
+
+export default ({ env }: { env: EnvFunction }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+  },
+  database: {
+    client: env("DATABASE_CLIENT", "sqlite"),
+    connection: {
+      filename: path.join(
+        __dirname,
+        "..",
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
+    },
+    useNullAsDefault: true,
   },
 });
